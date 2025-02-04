08248f16e9ca0889475480eb6dac34e7cf4dbc25
tinkering-townsperson <h2o.Drop2010@gmail.com>
2025-02-03 20:45:29 -0800

    feat: Add idle animation to Goose

--- scenes/main_character.gd
+++ scenes/main_character.gd
@@ -3,10 +3,12 @@ extends CharacterBody2D
 
 const SPEED = 400.0
 const JUMP_VELOCITY = -900.0
-@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var sprite_2d: AnimatedSprite2D = $Sprite2D
+
 
 
 func _physics_process(delta: float) -> void:
+	sprite_2d.animation = "default"
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -14,6 +16,11 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if velocity.y > 1:
+		sprite_2d.animation = "glide"
+	elif velocity.y < -1:
+		sprite_2d.animation = "jump"
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
