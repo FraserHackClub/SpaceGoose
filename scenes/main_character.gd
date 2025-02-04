340cba3abfaaf41786d10b44dafdd50acfd62cbd
Fengyuan (Harry) Liu <106412901+fengyuan66@users.noreply.github.com>
2025-02-03 21:12:19 -0800

    Add double jump / wall jump (#3)
    * feat: add propulsion asset
    
    * feat: add double jumps and wall jumps
    
    * chore: Fix typo
    
    ---------
    
    Co-authored-by: tinkering-townsperson <h2o.Drop2010@gmail.com>

--- scenes/main_character.gd
+++ scenes/main_character.gd
@@ -4,18 +4,28 @@ extends CharacterBody2D
 const SPEED = 400.0
 const JUMP_VELOCITY = -900.0
 @onready var sprite_2d: AnimatedSprite2D = $Sprite2D
+var jumpcount = 0
 
-
+func _ready():
+	$Sprite2D2.hide()
 
 func _physics_process(delta: float) -> void:
 	sprite_2d.animation = "default"
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	if is_on_floor():
+		jumpcount = 0
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and jumpcount < 2:
 		velocity.y = JUMP_VELOCITY
+		jumpcount += 1
+	
+	if Input.is_action_just_pressed("jump") and is_on_wall():
+		velocity.y = JUMP_VELOCITY
+		$Sprite2D2.show()
+		await get_tree().create_timer(0.2).timeout
+		$Sprite2D2.hide()	
 		
 	if velocity.y > 1:
 		sprite_2d.animation = "glide"
