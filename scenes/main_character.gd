5670c23fb55b0cba003f7ed4c43c2b36fd1f26dc
Fengyuan (Harry) Liu <106412901+fengyuan66@users.noreply.github.com>
2025-02-06 22:59:23 -0800

    Add ducking-walk animation, fix minor bugs, add speed debuff (#8)
    * feat: add ducking fixed sprite
    
    * fix: add ducking hitbox changes
    
    * fix: add ducking walking animation
    
    * feat: Ducking animation takes precedence over jump
    
    * feat: Change velocity when ducking
    
    ---------
    
    Co-authored-by: tinkering-townsperson <h2o.Drop2010@gmail.com>

--- scenes/main_character.gd
+++ scenes/main_character.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
-
 const SPEED = 400.0
 const JUMP_VELOCITY = -900.0
+const DUCKING_MULTIPLIER = 0.75
+
 @onready var sprite_2d: AnimatedSprite2D = $Sprite2D
 @onready var hitbox_normal = $CollisionShape2D
 @onready var hitbox_crouch = $CollisionShape2D_Duck
+
 var jumpcount = 0
 
 
@@ -27,46 +29,59 @@ func _process(delta):
 
 
 func _physics_process(delta: float) -> void:
-	sprite_2d.animation = "default"
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-	if is_on_floor():
-		jumpcount = 0
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and jumpcount < 2:
-		velocity.y = JUMP_VELOCITY
-		jumpcount += 1
+	var desired_anim = "default"
 	
+	# Add gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta * (1.5 if Input.is_action_pressed("down") else 1)
+	else:
+		jumpcount = 0
+
+	# Handle jumping.
+	if Input.is_action_just_pressed("jump") and jumpcount < 2:
+		velocity.y = JUMP_VELOCITY * (DUCKING_MULTIPLIER if Input.is_action_pressed("down") else 1)
+		jumpcount += 1
+
 	if Input.is_action_just_pressed("jump") and is_on_wall():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_VELOCITY * (DUCKING_MULTIPLIER if Input.is_action_pressed("down") else 1)
 		$Sprite2D2.show()
 		await get_tree().create_timer(0.2).timeout
-		$Sprite2D2.hide()	
-		
-	if Input.is_action_pressed("down"):
-		sprite_2d.animation = "duck"
-		# When crouching, disable the normal hitbox and enable the crouch hitbox.
+		$Sprite2D2.hide()
 
-		
-		
+	# Optionally override the animation based on vertical movement.
+	# (This block should only override if you really want these states to take precedence.)
 	if velocity.y > 1:
-		sprite_2d.animation = "glide"
+		desired_anim = "glide"
 	elif velocity.y < -1:
-		sprite_2d.animation = "jump"
+		desired_anim = "jump"
+	
+		# Determine if we're crouching.
+	if Input.is_action_pressed("down"):
+		hitbox_normal.disabled = true
+		hitbox_crouch.disabled = false
+		desired_anim = "duck"
+		if (velocity.x > 1 or velocity.x < -1) and (velocity.y == 0):
+			desired_anim = "sneak"
+	else:
+		hitbox_normal.disabled = false
+		hitbox_crouch.disabled = true
+		desired_anim = "default"
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Only change the animation if it isn't already playing.
+	if sprite_2d.animation != desired_anim:
+		sprite_2d.animation = desired_anim
+
+	# Handle horizontal movement.
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * SPEED * (DUCKING_MULTIPLIER if Input.is_action_pressed("down") else 1)
 	else:
 		velocity.x = move_toward(velocity.x, 0, 12)
 
 	move_and_slide()
-	
+
+	# Flip sprite based on direction.
 	if direction < 0:
 		sprite_2d.flip_h = true
 	elif direction > 0:
 		sprite_2d.flip_h = false
-	
