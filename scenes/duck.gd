261998027e515f7cde309f7f7d7a2b0021b5585b
HappyLOL-HL <fengyuan6666@gmail.com>
2025-02-13 19:54:27 -0800

    Fix: fix goose hitbox - no sitting on peak or butt

--- scenes/duck.gd
+++ scenes/duck.gd
@@ -7,13 +7,26 @@ extends CharacterBody2D
 var direction: Vector2 = Vector2.LEFT
 var is_falling: bool = false
 
+# Helper function to compute the bounding rectangle of a polygon
+func get_polygon_rect(polygon: PackedVector2Array) -> Rect2:
+	if polygon.is_empty():
+		return Rect2()
+	var min_x = polygon[0].x
+	var min_y = polygon[0].y
+	var max_x = polygon[0].x
+	var max_y = polygon[0].y
+	for point in polygon:
+		min_x = min(min_x, point.x)
+		min_y = min(min_y, point.y)
+		max_x = max(max_x, point.x)
+		max_y = max(max_y, point.y)
+	return Rect2(Vector2(min_x, min_y), Vector2(max_x - min_x, max_y - min_y))
+
 func _physics_process(delta: float) -> void:
 	if is_falling:
-
 		velocity = Vector2(0, fall_speed)
 		move_and_slide()
 		return
-
 
 	velocity.x = direction.x * speed
 	move_and_slide()
@@ -39,16 +52,19 @@ func _physics_process(delta: float) -> void:
 			goose_touching = true
 			goose = other
 			if goose.is_on_floor():
-				goose.call("game_over", 2)  
-				return  
+				goose.call("game_over", 2)
+				return			
+			# Get bounding rect for the goose's CollisionPolygon2D
+			var goose_poly: PackedVector2Array = goose.get_node("CollisionPolygon2D").polygon
+			var goose_rect = get_polygon_rect(goose_poly)
+			var goose_bottom = goose.global_position.y + (goose_rect.size.y / 2)
 			
 
-			var goose_bottom = goose.global_position.y + (goose.get_node("CollisionShape2D").shape.get_rect().size.y / 2)
-			var duck_top = global_position.y - (get_node("CollisionShape2D").shape.get_rect().size.y / 2)
+			var duck_rect = Rect2(-$CollisionShape2D.shape.extents, $CollisionShape2D.shape.extents * 2)
+			var duck_top = global_position.y - (duck_rect.size.y / 2)
 			
 			if goose_bottom <= duck_top:
 				goose_on_top = true
-	
 
 	if goose_touching and (goose_on_top or (goose and not goose.is_on_floor())):
 		start_falling()
@@ -59,7 +75,6 @@ func start_falling() -> void:
 	$CollisionShape2D.disabled = true
 	collision_layer = 0
 	collision_mask = 0
-
 
 	var timer = Timer.new()
 	timer.one_shot = true
