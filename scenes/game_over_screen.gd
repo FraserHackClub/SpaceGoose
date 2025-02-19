f0a30571f7de2b86bdc6611cc23695553eb93bfa
stickmanned <williamwen25@gmail.com>
2025-02-18 22:19:16 -0800

    feat: added deathscreen displaying goose skulls

--- scenes/game_over_screen.gd
+++ scenes/game_over_screen.gd
@@ -3,17 +3,17 @@ extends CanvasLayer
 const WIN = 1
 const LOSE = 2
 
-@onready var background_panel: Panel = $Panel
+@onready var panel: Panel = $Panel
 
+func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	self.layer = 100
+	self.follow_viewport_enabled = false  # Prevents the CanvasLayer from moving with the camera.
+	panel.set_anchors_preset(Control.PRESET_FULL_RECT, true)
+	panel.hide()
 
-func set_game_over_state(state: int):
-	if background_panel:
-		var style = background_panel.get_theme_stylebox("panel", "Panel") # Get the current StyleBox
-		if style is StyleBoxFlat: # Check if it's a StyleBoxFlat
-			if state == WIN:
-				style.bg_color = Color.GREEN
-			else:
-				style.bg_color = Color.RED
-			background_panel.add_theme_stylebox_override("panel", style)
-		else:
-			print("Warning: Panel's StyleBox is not a StyleBoxFlat.")
+func set_game_over_state(state: int) -> void:
+	if state == LOSE:
+		panel.show()
+	else:
+		panel.hide()
