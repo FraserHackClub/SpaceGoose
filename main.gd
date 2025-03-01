b90d56735d0d119621f4406b615fc40583813181
tinkering-townsperson <h2o.Drop2010@gmail.com>
2025-02-28 23:59:06 -0800

    feat: Move first level to separate scene

---
+++ main.gd
@@ -0,0 +1,15 @@
+extends Node
+
+
+func load_level(level_path):
+	get_tree().change_scene_to_file(level_path)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	load_level("res://scenes/worlds/world_1-1.tscn")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
