ad30906141e9938d82af7eaf6891b9ed7103f17e
Fengyuan (Harry) Liu <fengyuan6666@gmail.com>
2025-03-17 18:53:16 -0400

    Feat: Reloading
    Completed adding the gun, along with some of its UIs, made some parameters of the camera (screen width, etc) automatically adjust

--- scenes/player_gun.gd
+++ scenes/player_gun.gd
@@ -2,7 +2,13 @@ extends Node2D
 
 @onready var barrel_marker: Marker2D = $BarrelMarker2D
 @onready var SPRITE: AnimatedSprite2D = $AnimatedSprite2D2
+@onready var bullet_counter: Label = $"../../HUD/BulletCounter/BulletCountLabel"
+
+
+
 var BULLET = preload("res://scenes/bullet.tscn")
+var Bulletamount = int(30)
+
 
 var is_reloading = false  # Prevents shooting & animation override
 
@@ -23,7 +29,7 @@ func _process(delta: float) -> void:
 
 	if not is_reloading:
 		_handle_animation()
-
+	bullet_counter.text = str(Bulletamount)
 func _reload() -> void:
 	is_reloading = true
 	print("Setting animation to Reloading")
@@ -36,6 +42,8 @@ func _reload() -> void:
 	print("Waiting for reload animation:", reload_time, "seconds")
 
 	await get_tree().create_timer(reload_time).timeout  # Wait for the animation to finish
+	
+	Bulletamount = int(30)
 
 	print("Setting animation to Default")
 	SPRITE.animation = "AK47_Default"
@@ -44,13 +52,24 @@ func _reload() -> void:
 	is_reloading = false
 	print("Reload complete!")
 
-
+func shake(node: Node2D, duration: float = 1.0, intensity: float = 5.0):
+	var original_position = node.position  # Store original position
+	var shake_time = duration
+	while shake_time > 0:
+		shake_time -= 0.05  # Decrease shake time per iteration
+		node.position = original_position + Vector2(randf_range(-intensity, intensity), randf_range(-intensity, intensity))
+		await get_tree().create_timer(0.05).timeout  # Wait for a short time before shaking again
+	node.position = original_position  # Reset position after shaking
 
 func _shoot():
-	var bullet_instance = BULLET.instantiate()
-	get_tree().root.add_child(bullet_instance)
-	bullet_instance.global_position = barrel_marker.global_position
-	bullet_instance.rotation = rotation
+	if Bulletamount > 0:
+		var bullet_instance = BULLET.instantiate()
+		get_tree().root.add_child(bullet_instance)
+		bullet_instance.global_position = barrel_marker.global_position
+		bullet_instance.rotation = rotation
+		Bulletamount -= 1
+	else:
+		shake($AnimatedSprite2D2, 0.3, 5.0) #Time in seconds, Intensity
 
 func _handle_animation():
 	if is_reloading:
