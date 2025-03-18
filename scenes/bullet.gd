c546be0881156a0e7c22d6b19f77a973dc342909
tinkering-townsperson <h2o.Drop2010@gmail.com>
2025-03-17 23:21:37 -0700

    fix: Fix bullet logic

--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -20,10 +20,17 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 func _on_area_2d_area_entered(touchedarea: Node):
 	print("Bullet hit something:", touchedarea.name)
 	print("Groups:", touchedarea.get_groups())  
+	var enemy: Node = touchedarea.get_parent()  # Get the parent node (the full duck scene)
 
-	var enemy = touchedarea.get_parent()  # Get the parent node (the full duck scene)
 
-	if enemy and enemy.is_in_group("enemies"):  # Check if the parent is actually an enemy
+	if touchedarea and touchedarea.is_in_group("enemy"):
+		print("Enemy detected:", touchedarea.name)
+		touchedarea.start_falling()  # Call the function from the full enemy scene
+		queue_free()  # Destroy the bullet
+	elif enemy and enemy.is_in_group("enemy"):  # Check if the parent is actually an enemy
 		print("Enemy detected:", enemy.name)
 		enemy.start_falling()  # Call the function from the full enemy scene
 		queue_free()  # Destroy the bullet
+		
+	if touchedarea and touchedarea.name == "Terrain":
+		queue_free()
