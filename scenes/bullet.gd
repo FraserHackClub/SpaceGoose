337ae2abd0bd8b29a0aeebb4a33c8520f0ba9777
tinkering-townsperson <h2o.Drop2010@gmail.com>
2025-03-19 13:15:22 -0700

    fix: Fix bullet collisions with terrain and some enemies

--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -13,18 +13,25 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
 
 # Check if the area belongs to a duck
-func _on_area_2d_area_entered(touchedarea):
+func _on_area_2d_area_entered(touchedarea: Node):
 	print("Bullet hit something:", touchedarea.name)
 	print("Groups:", touchedarea.get_groups())  
 
-	var enemy = touchedarea.get_parent()  # Get the parent node (the full entity)
+	var enemy: Node = touchedarea.get_parent()  # Get the parent node (the full entity)
 
 	if enemy:
 		print("Parent detected:", enemy.name, " | Groups:", enemy.get_groups())
 
 	if enemy and enemy.is_in_group("enemy"):  
 		print("Enemy detected:", enemy.name)
-		enemy.start_falling()  
+		enemy.start_falling()
 		queue_free()  # Destroy the bullet
+	elif touchedarea and touchedarea.is_in_group("enemy"):
+		print("Enemy detected:", touchedarea.name)
+		touchedarea.start_falling()
+		queue_free()  # Destroy the bullet
+	elif touchedarea and touchedarea.name == "Terrain":
+		print("Terrain detected, breaking bullet")
+		queue_free()
 	else:
 		print("Not an enemy. Bullet will not trigger 'start_falling'.")
