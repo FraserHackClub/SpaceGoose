0b78b961d72ffc67e8f71b7bd45a3395f4f50650
tinkering-townsperson <h2o.Drop2010@gmail.com>
2025-03-19 11:24:35 -0700

    refactor: Remove empty lines and unused code in bullet.gd

--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -1,30 +1,18 @@
 extends Node2D
 
-#@onready var Duck = $"../duck"
-
 const SPEED: float = 3000.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position += transform.x * SPEED * delta
 
-
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
-	
 
-
-	# Check if the area belongs to a duck
-
-
-
-	
-
-
+# Check if the area belongs to a duck
 func _on_area_2d_area_entered(touchedarea):
 	print("Bullet hit something:", touchedarea.name)
 	print("Groups:", touchedarea.get_groups())  
