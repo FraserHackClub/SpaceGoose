d21875f56329221cee0ba2ed953c09cb5a9ad0b7
tinkering-townsperson <h2o.Drop2010@gmail.com>
2025-03-21 23:15:59 -0700

    fix: Make sure player spawns before other entities

--- scenes/worlds/world_1_1.gd
+++ scenes/worlds/world_1_1.gd
@@ -40,12 +40,12 @@ func _ready() -> void:
 	var egg_spawn_locations = [Vector2(1650, 526), Vector2(4640, 526)]
 	var duck_spawn_locations = [Vector2(1570, 496), Vector2(4550, 496)]
 	#var weapon_pickup_locations = [Vector2(1328, 496)]
+	Global.spawn_player(player_scene, current_scene, Vector2(0, 0), TIME, inventory, JUMP_VELOCITY)
 	Global.spawn_items(bread_scene, current_scene,  bread_spawn_locations)
 	#Global.spawn_items(weaponpickup_scene, current_scene,  weapon_pickup_locations)
 	Global.spawn_items(egg_scene, current_scene, egg_spawn_locations)
 	Global.spawn_enemies(duck_scene, current_scene, duck_spawn_locations)
 	Global.spawn_entity(finish_scene, current_scene, Vector2(4862, 439), "win_zone")
-	Global.spawn_player(player_scene, current_scene, Vector2(0, 0), TIME, inventory, JUMP_VELOCITY)
 	Global.spawn_camera(current_scene, LEVEL_LENGTH)
 	#Global.update_helmet_visibility()
 	emit_signal("level_ready")
