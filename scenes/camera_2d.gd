e22e48e62716cb9b95850f981eb486a779c2b91f
tinkering-townsperson <h2o.Drop2010@gmail.com>
2025-03-24 21:02:23 -0700

    feat: Add pause indicator in levels

--- scenes/camera_2d.gd
+++ scenes/camera_2d.gd
@@ -2,6 +2,7 @@ extends Camera2D
 
 @onready var goose: CharacterBody2D = $"../goose"
 @onready var camera_2d: Camera2D = $"."
+@onready var level: Node2D = $".."
 
 @export var smooth_speed: float = 0.1
 @export var LEVEL_LENGTH: float = 0.0
@@ -16,4 +17,9 @@ func _process(_delta: float) -> void:
 	if is_instance_valid(goose):
 		var target_x = min(max(0.0, goose.position.x), (LEVEL_LENGTH - VIEWPORT_WIDTH))
 		position.x = lerp(position.x, target_x, smooth_speed)
+	
+	if get_tree().paused:
+		$"status-indicator".animation = "pause"
+	else:
+		$"status-indicator".animation = "default"
 	
