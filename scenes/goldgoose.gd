b526021c72768ea79e2dc76c7ed2cc57df03baa9
William Wen <williamwen25@gmail.com>
2025-03-29 20:14:31 -0700

    feat: add gold egg collection

--- scenes/goldgoose.gd
+++ scenes/goldgoose.gd
@@ -1,1 +1,15 @@
 extends CharacterBody2D
+
+@onready var goose: CharacterBody2D = $"../goose"
+@onready var nest: AnimatedSprite2D = $"../Nest/AnimatedSprite2D"
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+
+func _on_collision_shape_2d_body_entered(body: Node) -> void:
+	# Check if the colliding body is the goose and if it has a gold egg
+	if body == goose and goose.inventory.has_gold_egg:
+		# Change the nest animation to "egg"
+		nest.animation = "egg"
+		# Change the gold goose's animation to "after"
+		animated_sprite.animation = "after"
--- scenes/worlds/world_1_1.gd
+++ scenes/worlds/world_1_1.gd
@@ -6,7 +6,7 @@ const egg_scene: PackedScene = preload("res://scenes/egg.tscn")
 const finish_scene: PackedScene = preload("res://scenes/finish.tscn")
 const player_scene: PackedScene = preload("res://scenes/main_character.tscn")
 const weaponpickup_scene: PackedScene = preload("res://scenes/WeaponPickup.tscn")
-
+const gold_goose_scene: PackedScene = preload("res://scenes/goldgoose.tscn")
 const LEVEL_LENGTH = 5056.0
 const TIME = 60.0
 const JUMP_VELOCITY = -900
@@ -44,6 +44,7 @@ func _ready() -> void:
 	#Global.spawn_items(weaponpickup_scene, current_scene,  weapon_pickup_locations)
 	Global.spawn_items(egg_scene, current_scene, egg_spawn_locations)
 	Global.spawn_enemies(duck_scene, current_scene, duck_spawn_locations)
+	Global.spawn_entity(gold_goose_scene, current_scene, Vector2(229,369))
 	Global.spawn_entity(finish_scene, current_scene, Vector2(4862, 439), "win_zone")
 	Global.spawn_camera(current_scene, LEVEL_LENGTH)
 	#Global.update_helmet_visibility()
