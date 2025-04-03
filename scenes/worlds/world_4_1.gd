ad4e83d77221f6b5d8ce596c7c9d73a5037c913e
stickmanned <williamwen25@gmail.com>
2025-04-02 23:16:40 -0700

    fix: doom duck

--- scenes/worlds/world_4_1.gd
+++ scenes/worlds/world_4_1.gd
@@ -12,7 +12,7 @@ const player_scene: PackedScene = preload("res://scenes/main_character.tscn")
 const weaponpickup_scene: PackedScene = preload("res://scenes/WeaponPickup.tscn")
 
 const LEVEL_LENGTH = 200000
-const TIME = 120
+const TIME = 1000
 const JUMP_VELOCITY = -900.0
 
 var rng = RandomNumberGenerator.new()
